861a401e8e5156c3ef7ce5a2f67c7b41500b12d9
HSUxAJ <79509075+HSUxAJ@users.noreply.github.com>
2023-05-28 01:03:16 +0800

    updata

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const list = document.getElementById('list');
-
 function createPerson(name) {
   const li = document.createElement('li');
   li.innerText = name;
 
   const deleteBtn = document.createElement('button');
   deleteBtn.innerText = '刪除';
+
   deleteBtn.addEventListener('click', () => {
-    li.classList.add('fade-out');
-    setTimeout(() => {
-      li.remove();
-    }, 300);
+    showConfirmationDialog(name, li);
   });
 
   li.appendChild(deleteBtn);
   list.appendChild(li);
 }
+
+function showConfirmationDialog(name, li) {
+  const confirmation = confirm(`確定要刪除 ${name} 嗎？`);
+  if (confirmation) {
+    li.remove();
+  }
+}
+
+// function addPerson() {
+//   const name = input.value.trim();
+
+//   if (name !== '') {
+//     createPerson(name);
+//     input.value = '';
+//     input.focus();
+//   }
+// }
+
+// addButton.addEventListener('click', addPerson);
+// input.addEventListener('keydown', (event) => {
+//   if (event.key === 'Enter') {
+//     addPerson();
+//   }
+// });
 
 const personNames = ['小明', '小華', '小李'];
 
