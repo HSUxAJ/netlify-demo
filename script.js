959488d07b4a3cfc45876ef5e4444a3fa66a00bd
HSUxAJ <79509075+HSUxAJ@users.noreply.github.com>
2023-05-28 23:34:27 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const list = document.getElementById('list');
 const input = document.getElementById('person-input');
-// const addButton = document.getElementById('add-btn');
 
 // 初始化 Firebase
 const firebaseConfig = {
@@ -18,7 +17,7 @@ firebase.initializeApp(firebaseConfig);
 // 取得 Firestore 的實例
 const firestore = firebase.firestore();
 
-function createPerson(name) {
+function createPerson(name, documentId) {
   const li = document.createElement('li');
   li.innerText = name;
 
@@ -26,17 +25,23 @@ function createPerson(name) {
   deleteBtn.innerText = '刪除';
 
   deleteBtn.addEventListener('click', () => {
-    showConfirmationDialog(name, li);
+    showConfirmationDialog(name, documentId, li);
   });
 
   li.appendChild(deleteBtn);
   list.appendChild(li);
 }
 
-function showConfirmationDialog(name, li) {
+function showConfirmationDialog(name, documentId, li) {
   const confirmation = confirm(`確定要刪除 ${name} 嗎？`);
   if (confirmation) {
-    li.remove();
+    // 刪除 Firestore 中的文件
+    firestore.collection("Tourist_Guide").doc(documentId).delete().then(() => {
+      console.log("文件已成功刪除");
+      li.remove(); // 從列表中刪除該項目
+    }).catch((error) => {
+      console.error("刪除文件時發生錯誤：", error);
+    });
   }
 }
 
@@ -46,14 +51,12 @@ const personNames = [];
 firestore.collection("Tourist_Guide").get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
     var data = doc.data();
-    personNames.push(data.name);
-    // console.log(data.name);
+    personNames.push({ name: data.name, id: doc.id }); // 將文件 ID 一併存儲
   });
 
   // Further operations relying on personNames can be performed here
   console.log(personNames);
-  personNames.forEach(name => createPerson(name));
+  personNames.forEach(({ name, id }) => createPerson(name, id));
 }).catch((error) => {
   console.log("Error fetching collection:", error);
 });
-
